3fb608a417e63e49c470e79d7b95d0b7ea3d290e
GustavoSCanova <62295167+GustavoSCanova@users.noreply.github.com>
2025-05-24 04:29:19 -0300

    Código Fonte

--- index.js
+++ index.js
@@ -6,3 +6,19 @@
       bot.classList.add("hide");
     }, 3000);
   });
+
+    const btnTopo = document.getElementById("btnTopo");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      btnTopo.style.display = "block";
+    } else {
+      btnTopo.style.display = "none";
+    }
+  });
+
+  // Rola suavemente para o topo quando clicado
+  btnTopo.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
